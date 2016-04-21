92dfa91c556bd6ad9349edebf04960f49a818007
Rich Pressler <richard.pressler@mesh01.com>
2016-04-20 20:33:15 -0400

    Auth logic started

--- app/components/login/login.component.js
+++ app/components/login/login.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,26 +10,35 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1;
     var LoginComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             LoginComponent = (function () {
-                function LoginComponent() {
+                function LoginComponent(_routeParams) {
+                    this._routeParams = _routeParams;
                 }
                 LoginComponent.prototype.ngOnInit = function () {
-                    window.location.href = 'https://github.com/login/oauth/authorize?client_id=c027f603d41d3cfe8e67';
+                    if (this._routeParams.params['code'] == undefined) {
+                        window.location.href = 'https://github.com/login/oauth/authorize?client_id=c027f603d41d3cfe8e67';
+                    }
+                    else {
+                        this.result = this._routeParams.get('code');
+                    }
                 };
                 LoginComponent = __decorate([
                     core_1.Component({
                         selector: 'login',
                         templateUrl: 'app/components/login/login.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [router_1.RouteParams])
                 ], LoginComponent);
                 return LoginComponent;
             }());
--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,15 +1,23 @@
 import {Component} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
 
 @Component({
   selector: 'login',
   templateUrl: 'app/components/login/login.component.html'
 })
 export class LoginComponent {
-  constructor() {
+  constructor(private _routeParams: RouteParams) {
 
   }
 
+  result: string;
+
   ngOnInit() {
-    window.location.href = 'https://github.com/login/oauth/authorize?client_id=c027f603d41d3cfe8e67';
+    if(this._routeParams.params['code'] == undefined) {
+      window.location.href = 'https://github.com/login/oauth/authorize?client_id=c027f603d41d3cfe8e67';
+    }
+    else {
+      this.result = this._routeParams.get('code');
+    }
   }
 }
